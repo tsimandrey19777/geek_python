643d67159060940a5dbd65330dc4f91a9a1f51df
tsimandrey19777 <siblp@mail.ru>
2019-06-10 06:31:08 +0800

    Доделал Hard и normal3

--- HW3/normal/3.py
+++ HW3/normal/3.py
@@ -2,12 +2,21 @@
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
-def filter_arr(arr, temp):
-    arr2 = []
-    for item in arr:
-        if item == temp:
-            arr2.append(item)
-    return arr2
 
-print(filter_arr(['a', 'c', '43', '3', 5, 10], 'c'))
+arr = ['a', 'c', '43', 'c', '3', 5, 10, ]
+temp = 'c'
 
+
+def filter_arr(item, temp):
+    if item == temp:
+        return True
+    else:
+        return False
+
+
+arr2 = []
+for item in arr:
+    if filter_arr(item, temp):
+        arr2.append(item)
+
+print(arr2)
